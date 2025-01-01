3c7663ae6dd47aa66fe3e7e8821f253db11b3ee3
Based <basedjamali@gmail.com>
2025-01-01 16:22:13 +0100

    Build the React Components

--- digital-clock-app/src/DigitalClock.jsx
+++ digital-clock-app/src/DigitalClock.jsx
@@ -1,10 +1,33 @@
 import React, {useState, useEffect} from "react";
 
 const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [time])
+
+  const formatTime = () => {
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+    let seconds = time.getSeconds()
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`
+  }
+
+  const padZero = (number) => {
+    return (number < 10 ? "0" : "") + number
+  }
+
   return(
     <div className="clock-container">
       <div className="clock">
-        <span>00:00:00</span>
+        <span>{formatTime()}</span>
       </div>
     </div>
   );
