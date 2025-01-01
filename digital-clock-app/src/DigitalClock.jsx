0710aa7a2dd1ec9ecb96aa9007c26832366fe1f2
Based <basedjamali@gmail.com>
2025-01-01 16:08:59 +0100

    Build the HTML Structure & Styled the CSS

--- digital-clock-app/src/DigitalClock.jsx
+++ digital-clock-app/src/DigitalClock.jsx
@@ -2,7 +2,11 @@ import React, {useState, useEffect} from "react";
 
 const DigitalClock = () => {
   return(
-    <></>
+    <div className="clock-container">
+      <div className="clock">
+        <span>00:00:00</span>
+      </div>
+    </div>
   );
 }
 
